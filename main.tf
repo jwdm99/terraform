7e28bae67e1a4edde51728568a550d5679e6d6b9
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-06 22:47:23 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -196,7 +196,6 @@ resource "azurerm_linux_virtual_machine" "JWM-VM-2" {
   resource_group_name = azurerm_resource_group.JWM-Terraform.name
   location            = azurerm_resource_group.JWM-Terraform.location
   size                = "Standard_B1s"
-  computer_name       = "JWM-Ubuntu"
   admin_username      = "superuser"
   network_interface_ids = [
     azurerm_network_interface.nic2.id,
