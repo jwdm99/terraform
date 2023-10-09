93391a868f1e973c87a29fe42504aae3beb8f570
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-09 15:16:31 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -225,7 +225,7 @@ resource "azurerm_linux_virtual_machine" "JWM-VM-2" {
 
 resource "azurerm_virtual_machine_extension" "test" {
   name                 = "test"
-  virtual_machine_id   = azurerm_linux_virtual_machine.JWM-VM-2.id
+  virtual_machine_id   = azurerm_linux_virtual_machine.test.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
