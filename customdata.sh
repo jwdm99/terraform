48a31f06270d9d2f0bcb502a4246d8332bc33ca8
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-09 16:04:41 +0000

    Add files via upload

--- customdata.sh
+++ customdata.sh
@@ -1,9 +1,6 @@
-sudo mkdir /myagent 
-cd /myagent
-sudo wget https://vstsagentpackage.azureedge.net/agent/2.179.0/vsts-agent-linux-x64-2.179.0.tar.gz
-sudo tar zxvf ./vsts-agent-linux-x64-2.179.0.tar.gz
-sudo chmod -R 777 /myagent
-runuser -l MyVmUser -c '/myagent/config.sh --unattended  --url https://dev.azure.com/MyDevOpsOrg --auth pat --token MyDevOpsToken --pool MyDevOpsPool'
-sudo /myagent/svc.sh install
-sudo /myagent/svc.sh start
+#!/bin/sh
+$ sudo apt update
+$ sudo apt install software-properties-common
+$ sudo add-apt-repository --yes --update ppa:ansible/ansible
+$ sudo apt install ansible
 exit 0
