a358eef6b129f85c01dc1cd0dd6b89d32c8719c5
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-09 23:22:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -229,7 +229,7 @@ resource "azurerm_virtual_machine_extension" "phase2" {
   virtual_machine_id   = azurerm_linux_virtual_machine.JWM-VM-2.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
-  type_handler_version = "2.1"
+  type_handler_version = "2.0"
 
   settings = <<SETTINGS
     {
