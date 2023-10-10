dd6ddba86d03a320d236444f5d15031349b5aac2
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-10 02:43:26 +0000

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -6,11 +6,14 @@ terraform {
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.10"
+      version = "1.9"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
+    }
+        spacelift = {
+      source = "spacelift.io/spacelift-io/spacelift"
     }
   }
 }
